0bf81370f6f2fb2e682b619675d49888f25be870
Kevin-kinyanjui <kevin.kinyanjui@student.moringaschool.com>
2023-06-05 11:07:52 +0300

    added context

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
-import React from "react";
+import React, { createContext, useState } from "react";
 import "./App.css";
+import Navbar from "./components/Navbar";
+
+const booksContext = createContext();
 
 function App() {
-  return <div className="App"></div>;
+  const [books, setBooks] = useState("works");
+  const [favoritebooks, setFavoriteBooks] = useState(null);
+  const [recommendations, setRecommendations] = useState(null);
+  const [readingList, setReadingList] = useState(null);
+  return (
+    <booksContext.Provider
+      value={{
+        books,
+        setBooks,
+        favoritebooks,
+        setFavoriteBooks,
+        recommendations,
+        setRecommendations,
+        readingList,
+        setReadingList,
+      }}
+    >
+      <div className="App">
+        <Navbar />
+      </div>
+    </booksContext.Provider>
+  );
 }
 
 export default App;
+export { booksContext };
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,38 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  NavLink,
+} from "react-router-dom";
+import BooksList from "./BooksList";
+import FavoriteBooks from "./FavoriteBooks";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      {" "}
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <NavLink to="/">Books</NavLink>
+              </li>
+              <li>
+                <NavLink to="favoritebooks">FavoriteBooks</NavLink>
+              </li>
+            </ul>
+          </nav>
+
+          <Routes>
+            <Route exact path="/" element={<BooksList />} />
+            <Route path="favoritebooks" element={<FavoriteBooks />} />
+          </Routes>
+        </div>
+      </Router>
+    </div>
+  );
 }
 
 export default Navbar;
