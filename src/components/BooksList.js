5acc9db9d853f7218452f5ce088d7c48fc07ddec
Mbogz <127532929+Mbogz@users.noreply.github.com>
2023-06-06 20:09:31 +0300

    Try this

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -6,5 +6,31 @@ function BooksList() {
   console.log(books);
   return <div>BookList</div>;
 }
+useEffect(() => {
+  fetch("https://example-data.draftbit.com/books?_limit=50")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Failed to fetch data");
+      }
+      return response.json();
+    })
+    .then((jsonData) => {
+      setData(jsonData);
+      setLoading(false);
+    })
+    .catch((error) => {
+      setError(error);
+      setLoading(false);
+    });
+}, []);
+
+if (loading) {
+  return <div>Loading...</div>;
+}
+
+if (error) {
+  return <div>Error: {error.message}</div>;
+}
+
 
 export default BooksList;
