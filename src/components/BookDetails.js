c431a6cc614ac27a77fb613f7fe8ef4b496e84a8
samuel <samuelkelvinmwangi@gmail.com>
2023-06-06 12:40:35 +0300

    adding the list to display title , gener, description, and date

--- src/components/BookDetails.js
+++ src/components/BookDetails.js
@@ -1,18 +1,22 @@
 import React from "react";
 
-
-
-//creating function for book list
-function BookList({ books, onItemClick }) {
+//creating function for book details( in list)
+function BookDetails({ books, onItemClick }) {
   function handleClick(bookId) {
     onItemClick(bookId);
   }
 
+//projects a list of the title, description....etc
   return (
     <ul>
       {books.map((book) => (
         <li key={book.id} onClick={handleClick.bind(null, book.id)}>
-          {book.title}
+                <h2>{book.title}</h2>
+      <p>Author: {book.author}</p>
+      <p>Genre: {book.genre}</p>
+      <p>Description: {book.description}</p>
+      <p>Publication Date: {book.publicationDate}</p>
+
         </li>
       ))}
     </ul>
