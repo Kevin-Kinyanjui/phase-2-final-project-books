f4c16b8ab7040e48b61e1fdbe69b3efbec59ad09
Njerimacharia98 <njerimacharia05@gmail.com>
2023-06-06 18:25:10 +0300

    profile image styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -33,7 +33,7 @@ function Navbar() {
                 <NavLink to="favoritebooks">Favorites</NavLink>
                 <NavLink to="Recommendations">Recommendations</NavLink>
                 <NavLink to="Active-reads">Active-reads</NavLink>
-                <NavLink to="comments">comments</NavLink>
+                <NavLink to="comments">Comments</NavLink>
                 
                 
 
@@ -52,9 +52,9 @@ function Navbar() {
         </div>
         <Routes>
           <Route exact path="/" element={<BooksList />} />
-          <Route path="favoritebooks" element={<Favorites />} />
-          <Route path="activereads" element={<Active-reads />} />
-          <Route path="comments" element={<Comments />} />
+          <Route path="favoritebooks" element={<FavoriteBooks />} />
+          {/* <Route path="activereads" element={<Active-reads />} />
+          <Route path="comments" element={<Comments />} /> */}
         </Routes>
       </Router>
     </>
