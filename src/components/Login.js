6d115a160b07f283dee2c9dba2b2ad8d734ff275
Kevin-kinyanjui <kevin.kinyanjui@student.moringaschool.com>
2023-06-06 10:44:49 +0300

    complete home page design view

--- src/components/Login.js
+++ src/components/Login.js
@@ -13,37 +13,42 @@ function Login() {
   }
 
   return (
-    <div className="loginBody">
-      <div class="container">
-        <h1 className="loginH1">Welcome to BookBuzz</h1>
-        <p className="loginP">
-          Embark on a literary journey like never before!
-        </p>
-        <p className="loginP">
+    <>
+      <div className="loginBody">
+        <div class="container">
+          <h1 className="loginH1">Welcome to BookBuzz</h1>
+          <h6 className="loginh6">
+            Embark on a literary journey like never before!
+          </h6>
+          <button class="button" onClick={handleClick}>
+            Sign in with google.
+          </button>
+        </div>
+      </div>
+      <>
+        <h5>ABOUT</h5>
+        <h6>
           Discover captivating tales, unlock hidden knowledge, and explore the
           vast realm of books with our revolutionary Library App.
-        </p>
-        <p className="loginP">
-          Immerse yourself in a world of infinite stories and unleash your inner
-          bookworm. Whether you're a casual reader, an avid bibliophile, or a
-          curious learner, our app has something for everyone.
-        </p>
-        <p className="loginP">But that's not all!</p>
-        <p className="loginP">
+        </h6>
+        <h6>
           Our advanced recommendation system will curate personalized book
-          suggestions tailored to your unique taste. Find your next favorite
-          read with ease and broaden your literary horizons.
-        </p>
-        <p className="loginP">
+          suggestions tailored to your unique taste.
+        </h6>
+        <h6>
+          Find your next favorite read with ease and broaden your literary
+          horizons.
+        </h6>
+        <h6>
           Indulge in the joy of literary exploration, connect with fellow book
-          enthusiasts, and share your thoughts on beloved stories. Like a book,
-          leave a mark, and become part of our vibrant community of readers.
-        </p>
-        <button class="button" onClick={handleClick}>
-          Sign in with google.
-        </button>
-      </div>
-    </div>
+          enthusiasts, and share your thoughts on beloved stories.
+        </h6>
+        <h6>
+          Like a book, leave a mark, and become part of our vibrant community of
+          readers.
+        </h6>
+      </>
+    </>
   );
 }
 
