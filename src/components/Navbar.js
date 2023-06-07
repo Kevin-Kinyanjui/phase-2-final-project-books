1bd3acda0a5dc5a9006f3ff7487135790c8f5281
Njerimacharia98 <njerimacharia05@gmail.com>
2023-06-07 11:18:37 +0300

    recent change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,67 @@
+// import React, { useContext } from "react";
+// import { booksContext } from "../App";
+// import { auth } from "../config/firebaseConfig";
+// import { signOut } from "firebase/auth";
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route,
+//   NavLink,
+// } from "react-router-dom";
+
+// import BooksList from "./BooksList";
+// import FavoriteBooks from "./FavoriteBooks";
+
+// function Navbar() {
+//   let { user, setUser } = useContext(booksContext);
+
+//   function logOut() {
+//     signOut(auth).then(() => {
+//       setUser(null);
+//     });
+//   }
+
+//   return (
+//     <>
+//       <Router>
+//         <div className="navbar">
+//           <div className="links">
+//             <nav>
+//               <ul>
+//                 <NavLink to="/">Home</NavLink>
+
+//                 <NavLink to="favoritebooks">Favorites</NavLink>
+//                 <NavLink to="Recommendations">Recommendations</NavLink>
+//                 <NavLink to="Active-reads">Active-reads</NavLink>
+//                 <NavLink to="comments">Comments</NavLink>
+                
+                
+
+
+//               </ul>
+//             </nav>
+//           </div>
+
+//           <div className="user">
+//             <p>{user?.displayName}</p>
+//             {user && (
+//               <img src={user?.photoURL} alt="user" width="40" height="40" />
+//             )}
+//           </div>
+//           <button onClick={logOut}>logout</button>
+//         </div>
+//         <Routes>
+//           <Route exact path="/" element={<BooksList />} />
+//           <Route path="favoritebooks" element={<FavoriteBooks />} />
+//           {/* <Route path="activereads" element={<Active-reads />} />
+//           <Route path="comments" element={<Comments />} /> */}
+//         </Routes>
+//       </Router>
+//     </>
+//   );
+// }
+
+// export default Navbar;
 import React, { useContext } from "react";
 import { booksContext } from "../App";
 import { auth } from "../config/firebaseConfig";
