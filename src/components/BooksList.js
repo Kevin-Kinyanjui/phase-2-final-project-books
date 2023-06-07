c64d39422c2dab8a6742365cb3ff04f9a91ef9b6
Kevin-kinyanjui <kevin.kinyanjui@student.moringaschool.com>
2023-06-07 11:29:31 +0300

    fixing errors

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -1,49 +1,50 @@
 import React, { useContext } from "react";
 import { booksContext } from "../App";
-import React, { useEffect } from "react";
 
+// const BooksList = ({ books, onBookOver }) => {
+//   let { books, setBooks } = useContext(booksContext);
 
-const BooksList = ({ books, onBookOver }) => {
-  let { books, setBooks } = useContext(booksContext);
+//   useEffect(() => {
+//     fetch("../db.json")
+//       .then((response) => {
+//         if (!response.ok) {
+//           throw new Error("Failed to fetch data");
+//         }
+//         return response.json();
+//       })
+//       .then((jsonData) => {
+//         setBooks(jsonData);
+//         setLoading(false);
+//       })
+//       .catch((error) => {
+//         setError(error);
+//         setLoading(false);
+//       });
+//   }, []);
 
-  useEffect(() => {
-    fetch("../db.json")
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Failed to fetch data");
-        }
-        return response.json();
-      })
-      .then((jsonData) => {
-        setBooks(jsonData);
-        setLoading(false);
-      })
-      .catch((error) => {
-        setError(error);
-        setLoading(false);
-      });
-  }, []);
-  
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-  
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  }
-  
-};
+//   if (loading) {
+//     return <div>Loading...</div>;
+//   }
 
-  return (
-    <ul className="book-list">
-      {books.map((book) => (
-        <li key={book.id} onOver={() => onBookOver(book.id)}>
-          {book.title}
-        </li>
-      ))}
-    </ul>
-  );
+//   if (error) {
+//     return <div>Error: {error.message}</div>;
+//   }
 
-  
+// };
+
+//   return (
+//     <ul className="book-list">
+//       {books.map((book) => (
+//         <li key={book.id} onOver={() => onBookOver(book.id)}>
+//           {book.title}
+//         </li>
+//       ))}
+//     </ul>
+//   );
+
+function BooksList() {
+  // let { books, setBooks } = useContext(booksContext);
+  return <></>;
+}
 
 export default BooksList;
