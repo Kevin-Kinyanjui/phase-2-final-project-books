2cbcf9f13f2f01f01903aed720276e1442aaddb8
Njerimacharia98 <njerimacharia05@gmail.com>
2023-06-07 15:21:36 +0300

    css updates

--- src/components/Book.js
+++ src/components/Book.js
@@ -2,8 +2,12 @@ import React from "react";
 
 function Book({ book }) {
   return (
-    <div>
+    <div className="Book">
+
+      
+      <img src={book.image_url} alt="book" width={200} height={300}/>
       <div>{book.title}</div>
+      <div>{book.authors}</div>
     </div>
   );
 }
