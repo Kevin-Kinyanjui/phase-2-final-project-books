365bec079934acde59e48166f0d81c832a944078
Kevin-kinyanjui <kevin.kinyanjui@student.moringaschool.com>
2023-06-07 12:35:40 +0300

    fix booklist

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -1,8 +1,38 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { booksContext } from "../App";
 
 function BooksList() {
-  // let { books, setBooks } = useContext(booksContext);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  let { books, setBooks } = useContext(booksContext);
+
+  useEffect(() => {
+    fetch("https://example-data.draftbit.com/books?_limit=50")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to fetch data");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setBooks(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setLoading(false);
+      }); // eslint-disable-next-line
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  console.log(books);
   return <></>;
 }
 
