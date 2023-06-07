4724b0a0d2b79b0410ce3a322aed0acbfb80802f
Kevin-kinyanjui <kevin.kinyanjui@student.moringaschool.com>
2023-06-07 11:43:13 +0300

    minor edits

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -1,47 +1,6 @@
 import React, { useContext } from "react";
 import { booksContext } from "../App";
 
-// const BooksList = ({ books, onBookOver }) => {
-//   let { books, setBooks } = useContext(booksContext);
-
-//   useEffect(() => {
-//     fetch("../db.json")
-//       .then((response) => {
-//         if (!response.ok) {
-//           throw new Error("Failed to fetch data");
-//         }
-//         return response.json();
-//       })
-//       .then((jsonData) => {
-//         setBooks(jsonData);
-//         setLoading(false);
-//       })
-//       .catch((error) => {
-//         setError(error);
-//         setLoading(false);
-//       });
-//   }, []);
-
-//   if (loading) {
-//     return <div>Loading...</div>;
-//   }
-
-//   if (error) {
-//     return <div>Error: {error.message}</div>;
-//   }
-
-// };
-
-//   return (
-//     <ul className="book-list">
-//       {books.map((book) => (
-//         <li key={book.id} onOver={() => onBookOver(book.id)}>
-//           {book.title}
-//         </li>
-//       ))}
-//     </ul>
-//   );
-
 function BooksList() {
   // let { books, setBooks } = useContext(booksContext);
   return <></>;
