d9e28d4a4f4195366f8fd801613886c1207fe048
Kevin-kinyanjui <kevin.kinyanjui@student.moringaschool.com>
2023-06-07 11:40:59 +0300

    navbar complete

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,6 +11,9 @@ import {
 
 import BooksList from "./BooksList";
 import FavoriteBooks from "./FavoriteBooks";
+import Recommendations from "./Recommendations";
+import ReadingList from "./ReadingList";
+import Comments from "./Comments";
 
 function Navbar() {
   let { user, setUser } = useContext(booksContext);
@@ -32,13 +35,8 @@ function Navbar() {
 
                 <NavLink to="favoritebooks">Favorites</NavLink>
                 <NavLink to="Recommendations">Recommendations</NavLink>
-                <NavLink to="Active-reads">Active-reads</NavLink>
+                <NavLink to="readinglist">Active-reads</NavLink>
                 <NavLink to="comments">Comments</NavLink>
-                <NavLink to="Search">Search</NavLink>
-                
-                
-
-
               </ul>
             </nav>
           </div>
@@ -54,7 +52,9 @@ function Navbar() {
         <Routes>
           <Route exact path="/" element={<BooksList />} />
           <Route path="favoritebooks" element={<FavoriteBooks />} />
-    
+          <Route path="recommendations" element={<Recommendations />} />
+          <Route path="readinglist" element={<ReadingList />} />
+          <Route path="comments" element={<Comments />} />
         </Routes>
       </Router>
     </>
