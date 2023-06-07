65fd1825619b81a2a9a8a2feda22c1d9b7060ac7
Kevin-kinyanjui <kevin.kinyanjui@student.moringaschool.com>
2023-06-07 19:46:57 +0300

    minor edits

--- src/components/Book.js
+++ src/components/Book.js
@@ -6,9 +6,7 @@ function Book({ book }) {
       <img src={book.image_url} alt="book" width={200} height={300} />
       <div>{book.title}</div>
       <div>{book.authors}</div>
-      <button onClick={handleLike} disabled={liked}>
-        Like
-      </button>
+      <button>Like</button>
     </div>
   );
 }
