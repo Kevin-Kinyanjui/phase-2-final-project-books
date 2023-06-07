5083fac44f9e826803b7b70b47dc78340a50792c
Mbogz <127532929+Mbogz@users.noreply.github.com>
2023-06-07 20:16:22 +0000

    changed

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,29 @@
 import React from "react";
+const books = [
+  { title: "Book A", author: "Author A" },
+  { title: "Book B", author: "Author B" },
+  { title: "Book C", author: "Author C" },
+];
 
 function SearchBar() {
+  const searchInput = document.getElementById("searchInput").value;
+  const searchResult = document.getElementById("searchResult");
+  const filteredBooks = books.filter(book =>
+      book.title.toLowerCase().includes(searchInput.toLowerCase())
+    );
+    if (filteredBooks.length === 0) {
+      searchResult.innerHTML = "No books found.";
+    } else {
+      filteredBooks.forEach(book => {
+        const bookDiv = document.createElement("div");
+        bookDiv.innerHTML = `<strong>Title:</strong> ${book.title}<br><strong>Author:</strong> ${book.author}<br><br>`;
+        searchResult.appendChild(bookDiv);
+      });
+    }
+  
+  const searchButton = document.getElementById("searchButton");
+   searchButton.addEventListener("click", SearchBar);
+
   return (
     <>
       <div class="search-container">
@@ -9,5 +32,6 @@ function SearchBar() {
     </>
   );
 }
+
 
 export default SearchBar;
