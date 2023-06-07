87f6dafcfec7b762853edcc32d8f962796ee213e
Mbogz <127532929+Mbogz@users.noreply.github.com>
2023-06-07 19:42:57 +0300

    changes made

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,10 +1,16 @@
 import React, { useContext } from "react";
-import { booksContext } from "../App";
 
-function Book() {
-  let { books } = useContext(booksContext);
-  console.log(books);
-  return <div>Books</div>;
+function Book({ book }) {
+  return (
+    <div className="Book">
+
+      
+      <img src={book.image_url} alt="book" width={200} height={300}/>
+      <div>{book.title}</div>
+      <div>{book.authors}</div>
+    </div>
+  );
 }
+
 
 export default Book;
--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -1,48 +1,47 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect,useState } from "react";
 import { booksContext } from "../App";
-import React, { useEffect } from "react";
+import Book from "./Book";
 
-
-const BooksList = ({ books, onBookOver }) => {
+function BooksList() {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
   let { books, setBooks } = useContext(booksContext);
 
   useEffect(() => {
-    fetch("../db.json")
+    fetch("https://example-data.draftbit.com/books?_limit=150")
       .then((response) => {
         if (!response.ok) {
           throw new Error("Failed to fetch data");
         }
         return response.json();
       })
-      .then((jsonData) => {
-        setBooks(jsonData);
+      .then((data) => {
+        setBooks(data);
         setLoading(false);
       })
       .catch((error) => {
         setError(error);
         setLoading(false);
-      });
+      }); // eslint-disable-next-line
   }, []);
-  
+
   if (loading) {
     return <div>Loading...</div>;
   }
-  
+
   if (error) {
     return <div>Error: {error.message}</div>;
   }
-  
-};
 
+  console.log(books);
   return (
-    <ul className="book-list">
+    <>
       {books.map((book) => (
-        <li key={book.id} onOver={() => onBookOver(book.id)}>
-          {book.title}
-        </li>
+        <Book key={book.id} book={book} />
       ))}
-    </ul>
+    </>
   );
+}
 
   
 
